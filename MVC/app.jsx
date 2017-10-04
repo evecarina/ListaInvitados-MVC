7058c4a0f2f57916d20f2a07d9b62f07c0936b4c
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-10-04 13:48:30 -0500

    añade nuevo invitado

--- MVC/app.jsx
+++ MVC/app.jsx
@@ -1,73 +1,64 @@
 class Model{
-  subscribe(render){
-    this.callback = render;
-  }
-  notify() {
-    this.callback();
- }
-
- createLI(){
-    
-  const li = document.createElement('li');
-  li.textContent = text;
-  const label = document.createElement('label');
-  label.textContent = 'Confirmed';
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  label.appendChild(checkbox);
-  li.appendChild(label);  
-  const button = document.createElement('button');
-  button.textContent = 'remove';
-  li.appendChild(button);
-  // return li;
-   this.callback = render;
-   this.callback();
+    constructor(){
+        this.guests=[];
+        this.input=null;
+        this.callback=null;
+    }
+     addGuests(){
+      this.guests.push({name:this.input.value,id:Utils.uuid()});
+      this.input.value='';
+      this.update();
+    }
+    subscribe(render){
+        this.callback=render;
+    }
+    update(){
+       this.callback();
+     }
  
 }
+const ListGuests=({model})=>{
+  return (<ul>
+    {model.guests.map((g)=>{
+      return <li><label>Confirmed<input type='checkbox' /></label><button>Remove</button></li>;
+      })
+    }
+    </ul>);
 }
 
-
-const SubmitGuests = ({model})=>{
-  return ( <header>
-            <h1>RSVP</h1>
-            <p> Registration App </p>
-            <form id="registrar" >   
-            <input type="text" onChange={e => (model.input = e.target)}/>
-            <button type="submit"  name="submit" value="submit" >Submit</button>
-            </form>
-           </header>);    
-}    
-
-const createList=({})=>{
-  return();
-}    
-            
-              
- 
-
-
-
- 
-
-
-
-
-
-const UserView=({title,model})=>{
+const SubmitGuests=({title,model})=>{
   return (
-    <div className="scoreboard">
-     
-     <SubmitGuests model={model}/>
-     </div>
+    <div className="wrapper">
+      <header>
+        <h1>RSVP</h1>
+        <p> Registration App </p>
+        <form onSubmit={(e)=>{e.preventDefault();model.addGuests()}}>
+          <input type="text" onChange={e => (model.input = e.target)} />
+          <button type="submit">Submit</button>
+        </form>
+      </header>
+      <div className="main">	
+        <h2>Invitees</h2>
+        <ListGuests model={model}/>
+      </div>
+    </div>
+  );
+}
+
+ const UserView=({title,model})=>{
+  return (
+    <div className="scoreboard">     
+    <SubmitGuests model={model}/>
+    </div>
   );
 }
 
 let model = new Model();
 let render = () => {
    ReactDOM.render(
-      <UserView title="UserView" model={model} />,
+      < UserView title="UserView" model={model} />,
       document.getElementById('container')
    );
 };
 model.subscribe(render);
-render(); 
+render();  
